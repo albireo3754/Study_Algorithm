e49fbe700df2bcef7a63c2965799d6c67d2ceee4
starpro123 <40790219+starpro123@users.noreply.github.com>
2020-10-31 23:38:26 +0900

    Create 15_Triangle.py

---
+++ codility/15_Triangle.py
@@ -0,0 +1,10 @@
+def solution(A):
+    # write your code in Python 3.6
+
+    A.sort()
+    
+    for i in range(2, len(A)):
+        if A[i] < A[i-1] + A[i-2]:
+            return 1
+    
+    return 0
