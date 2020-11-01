3a56ac8c68d2222ea4db323946a3300600686d74
unknown <starpro123@naver.com>
2020-11-01 14:42:05 +0900

    100%

--- codility/18_Brackets.py
+++ codility/18_Brackets.py
@@ -1,15 +1,23 @@
 #1 - 62% 
+#why i got 62% score? -> don't check if parenthenis ends at left bracket
+#2 - 100%
 
 def solution(S):
     # write your code in Python 3.6
     brackets = []
 
     for bracket in S:
-        if (bracket == "{") or (bracket == "[") or (bracket == "{"):
+        if (bracket == "{") or (bracket == "[") or (bracket == "("):
             brackets.append(bracket)
         else:
-            compBra = {"}" : "{", "]" : "[", ")" : "("}[brackets.pop()] 
+            if len(brackets) == 0:
+                return 0
+                
+            compBra = {"{" : "}", "[" : "]", "(" : ")"}.get(brackets.pop()) 
             if compBra != bracket:
                 return 0
 
-    return 1
+    if len(brackets) == 0:
+        return 1
+    else:
+        return 0
