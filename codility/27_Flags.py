90a0dc5ed8279fa4a4523585d9dd9c353549e84b
unknown <starpro123@naver.com>
2020-11-13 00:16:00 +0900

    93

--- codility/27_Flags.py
+++ codility/27_Flags.py
@@ -1,4 +1,6 @@
 # score 73
+# 2nd - 93 score - time out error in large_anti_slow - why?
+
 import math
 
 
@@ -9,27 +11,31 @@ def solution(A):
         if A[i] > A[i-1] and A[i] > A[i + 1]:
             peaks.append(i)
 
-    maxFlag2 = int(math.sqrt(len(A)))
+    maxFlag2 = int(math.sqrt(len(A)))+1
     maxFlag = 0
-    print(peaks, maxFlag2)
+
+    if len(peaks) == 0:
+        return 0
 
     while maxFlag2 > 0:
-        if maxFlag > maxFlag2:
+        if maxFlag >= maxFlag2:
             return maxFlag
         beforePeak = peaks[0]
         flag = 1
         idx = 1
-        while maxFlag2 >= flag and idx < len(peaks):
+        while maxFlag2 > flag and idx < len(peaks):
             peak = peaks[idx]
             if peak - beforePeak >= maxFlag2:
                 flag += 1
                 beforePeak = peak
             idx += 1
-        maxFlag = flag
+        maxFlag = max(flag, maxFlag)
         maxFlag2 -= 1
-        print(maxFlag2)
 
     return flag
+
+
+solution([1])
 
 
 solution([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])
