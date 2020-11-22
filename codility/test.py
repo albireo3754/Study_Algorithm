6cf536529c01922097aca343cdebed011cb54c1e
unknown <starpro123@naver.com>
2020-11-22 21:29:42 +0900

    100

--- codility/test.py
+++ codility/test.py
@@ -1,36 +1,45 @@
-# score 73
-import math
-
+# 54%
 
 def solution(A):
     # write your code in Python 3.6
     peaks = []
-    for i in range(1, len(A)-1):
-        if A[i] > A[i-1] and A[i] > A[i + 1]:
-            peaks.append(i)
 
-    maxFlag2 = int(math.sqrt(len(A)))
-    maxFlag = 0
+    for i in range(1, len(A)-1):
+        if A[i-1] < A[i] and A[i] > A[i+1]:
+            peaks.append(i)
 
     if len(peaks) == 0:
         return 0
 
-    while maxFlag2 > 0:
-        if maxFlag >= maxFlag2:
-            return maxFlag
-        beforePeak = peaks[0]
-        flag = 1
-        idx = 1
-        while maxFlag2 > flag and idx < len(peaks):
-            peak = peaks[idx]
-            if peak - beforePeak >= maxFlag2:
-                flag += 1
-                beforePeak = peak
-            idx += 1
-        maxFlag = max(flag, maxFlag)
-        maxFlag2 -= 1
+    block = len(peaks) + 1
+    if peaks[-1] + 1 <= len(A) // 2:
+        return 1
+    while block > 1:
+        block -= 1
+        blockStart = 0
+        blockEnd = -1
 
-    return flag
+        if len(A) % block != 0:
+            continue
 
+        K = len(A) // block
 
-solution([1])
+        k = 0
+        t = 1
+        for i in range(1, block+1):
+            blockStart = blockEnd + 1
+            blockEnd = i * K - 1
+            for j in range(k, len(peaks)):
+                if blockEnd >= peaks[j]:
+                    if blockStart <= peaks[j]:
+                        k = j + 1
+                        break
+                else:
+                    t = 0
+                    break
+            if t == 0:
+                break
+
+        if t == 0:
+            continue
+        return block
