7f9e4bd0c0b2cd0b40cb0604b64bfd3667a7af11
starpro123 <40790219+starpro123@users.noreply.github.com>
2020-11-25 00:19:23 +0900

    55

--- codility/31_CountNonDivisible.py
+++ codility/31_CountNonDivisible.py
@@ -1,15 +1,28 @@
 # score 55 - O(N**2)
+# second code is 55 also.
 
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+import math
+
 def solution(A):
     # write your code in Python 3.6
+    aMax = 0
+    numCnts = [0 for _ in range(len(A) * 2 + 1)]
     nonDivs = [0 for _ in A]
+
+    for i in A:
+        numCnts[i] += 1
     
-    for i in range(0, len(A)):
-        for j in range(0, len(A)):
-            if A[j] % A[i] != 0:
-                nonDivs[j] += 1
+    for i in range(len(A)):
+        div = 0
+        j = 1
+        while j <= A[i]:
+            if A[i] % j == 0:
+                div += numCnts[j]
+            j+= 1
     
+        nonDivs[i] = len(A) - div
+        
     return nonDivs
