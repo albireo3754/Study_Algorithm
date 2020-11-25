aabf5db338b60b812a4b949c9eb7601740a8279d
starpro123 <40790219+starpro123@users.noreply.github.com>
2020-11-25 23:23:50 +0900

    Create 32.py

---
+++ codility/32.py
@@ -0,0 +1,50 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N, P, Q):
+    # write your code in Python 3.6
+    
+    # N is max integer
+    # M is p.len()
+    M = len(P)
+
+    primes = [0 for _ in range(N + 1)]
+    primes[1] = 1
+    prime = []
+
+    i = 2
+    while i <= int(N**0.5):
+        if primes[i] == 0:
+            primes[i] = 0
+            prime.append(i)
+            j = 2
+            while i * j <= N:
+                primes[i * j] = i
+                j += 1
+        i += 1
+
+    print(primes)
+    # for i in range(2,len(primes)):
+    #     if primes[i] == 0:
+    #         prime.append(i)
+        
+    semiPri = [0 for _ in range(N + 1)]
+    print(prime)
+    for i in prime:
+        print(i)
+        j = 4
+        while j<=N:
+            if j % i == 0:
+                if primes[j//i] == 0:
+                    semiPri[j] = 1
+            j += 1
+
+    print(semiPri)
+    query = [0 for _ in range(N+1)]
+    for i in range(1,len(semiPri)):
+        query[i] = query[i-1] + semiPri[i]
+    print(query)
+    return [query[Q[i]] - query[P[i]] for i in range(len(P))]
+
+
+    
