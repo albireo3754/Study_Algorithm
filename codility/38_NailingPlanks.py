c09661bfdfadc122e4d86cf4dd039c0b9a46bc99
starpro123@naver.com <starpro123@gmail.com>
2020-12-06 05:17:56 +0000

    fake 100

--- codility/38_NailingPlanks.py
+++ codility/38_NailingPlanks.py
@@ -1,50 +1,51 @@
 # 1st. O((N + M) * N) score:  50
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-def checkNailed(A,B,C):
-    N = len(A)
-    M = len(C)
-    idxStack = [i for i in range(N)]
-    delNode = []
-    for nail in C:
-        for i in idxStack:
-            # print(i)
-            if A[i] <= nail and B[i] >= nail:
-                delNode.append(i)
-
-        for j in delNode:
-            idxStack.remove(j)
-        delNode =[]
-
-        if len(idxStack) == 0:
-            return True
-
-    return False
-
+# 2nd. by https://codesays.com/2014/solution-to-nailing-planks-by-codility/ 
+# clear this problem fake 100
 # print(checkNailed([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10]))
 
+def findFirstNail(plankBegin, plankEnd, nails, preResult):
+    result = -1
+    resultPos = -1
+    nailLower = 0
+    nailUpper = len(nails) - 1
+    nailMid = 0
+
+    while nailLower <= nailUpper:
+        nailMid = (nailLower + nailUpper) // 2
+        nailPosMid = nails[nailMid][1]
+
+        if nailPosMid < plankBegin:
+            nailLower = nailMid + 1
+        elif nailPosMid > plankEnd:
+            nailUpper = nailMid - 1
+        else:
+            nailUpper = nailMid - 1
+            result = nails[nailMid][0]
+            resultPos = nailMid
+            print(result,nailMid)
+    print("result")
+    if result == -1:
+        return -1
+    
+    resultPos += 1
+
+    while resultPos < len(nails):
+        if nails[resultPos][1] > plankEnd:
+            break
+        result = min(result, nails[resultPos][0])
+        resultPos += 1
+        if preResult >= result:
+            return preResult
+    return max(result, preResult)
 
 def solution(A, B, C):
     # write your code in Python 3.6
+    nails = sorted(enumerate(C), key = lambda x: x[1])
+    result = -1
     N = len(A)
-    M = len(C)
-    
-    startIdx = 0
-    endIdx = M
-    resultIdx = -1
-    
-    while startIdx <= endIdx:
-        print(startIdx,endIdx)
-        midIdx = (startIdx + endIdx) // 2  
-        # print(midIdx, startIdx, endIdx)  
-        if checkNailed(A,B,C[:midIdx]):
-            resultIdx = midIdx
-            # print(resultIdx, M,midIdx,"result")
-            endIdx = midIdx - 1
-        else:
-            startIdx = midIdx + 1
-               
-    return resultIdx
 
-print(solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2]))
+    for plankIndex in range(N):
+        result = findFirstNail(A[plankIndex], B[plankIndex], nails, result)
+        if result == -1:
+            return result
+    return result + 1
