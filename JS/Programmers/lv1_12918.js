8fae86e9c563b4b0ab26d9477e93c3314788150a
starpro123 <starpro123@naver.com>
2021-01-10 12:53:26 +0000

    lv1_12918.js

--- JS/Programmers/lv1_12918.js
+++ JS/Programmers/lv1_12918.js
@@ -1,9 +1,8 @@
 function solution(s) {
   if (s.length !== 4 && s.length !== 6) return false;
-  else {
-    for (let v of s) {
-      if (isNaN(v)) return false;
-    }
-    return true;
+
+  for (let v of s) {
+    if (Number.isNaN(v)) return false;
   }
+  return true;
 }
