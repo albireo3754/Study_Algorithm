2dc202da6fc0e254177bbef728c77e62e97249a3
unknown <starpro123@naver.com>
2021-02-07 13:18:34 +0900

    Dfs

---
+++ Python/backjoon/silver/2606.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+N = int(input())
+edge_N = int(input())
+
+edges = defaultdict(list)
+
+for i in range(edge_N):
+    x, y = map(int,input().split())
+    edges[x].append(y)
+    edges[y].append(x)
+
+print(edges)
+visited = [0 for i in range(N + 1)]
+
+def dfs(node):
+    if visited[node]:
+        return
+    visited[node] = 1
+
+    for i in edges[node]:
+        dfs(i)
+
+dfs(1)
+
+print(sum(visited))
