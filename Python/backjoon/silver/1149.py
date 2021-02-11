359e1673c075de1e821f718efea5f4993f14bf4b
unknown <starpro123@naver.com>
2021-02-11 13:22:39 +0900

    DP

---
+++ Python/backjoon/silver/1149.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+rgb = []
+
+for i in range(N):
+    rgb.append(list(map(int, input().split(' '))))
+
+for i in range(1, N):
+    for j in range(3):
+        rgb[i][j] += min((rgb[i - 1][:j] + rgb[i - 1][j + 1:]))
+
+print(min(rgb[2]))
