d7ff489a052ed02fcfdcc39841a55477332a0217
unknown <starpro123@naver.com>
2021-02-13 01:39:16 +0900

    DP

--- Python/backjoon/silver/1932.py
+++ Python/backjoon/silver/1932.py
@@ -6,7 +6,6 @@ n = int(input())
 triangle = [[]]
 for i in range(1, n + 1):
     triangle.append(list(map(int, input().split(' '))))
-print(triangle)
 
 for i in range(2, n + 1):
     for j in range(0, i):
