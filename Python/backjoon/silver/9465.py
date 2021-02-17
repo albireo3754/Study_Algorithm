a21541f8100c130439d0dbb1af94a5f4fc2fcd41
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-02-17 22:57:16 +0900

    DP

---
+++ Python/backjoon/silver/9465.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+for i in range(T):
+    n = int(input())
+    sticker = [list(map(int, input().split())) for i in range(2)]
+    print(sticker)
+
+    score = [[0 for _ in range(n)] for _ in range(2)]
+    reverse = [1, 0]
+    for j in range(n):
+        for i in range(2):
+            
+            if j == 0:
+                score[i][j] = sticker[i][j]
+            else:
+                score[i][j] = sticker[i][j] + max(score[reverse[i]][j - 1], score[reverse[i]][j - 2])
+    print(max(score[0][n - 1], score[1][n - 1]))
