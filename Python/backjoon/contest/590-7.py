7e1e8e98540635ae0664431f3be97d578a0b731e
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-02-22 01:53:01 +0900

    DP

--- Python/backjoon/contest/590-7.py
+++ Python/backjoon/contest/590-7.py
@@ -1,33 +1,45 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000000)
 N, T = map(int, input().split(' '))
-exps = []
-for i in range(N):
+exps = [(0, 0)]
+for i in range(1, N + 1):
     ci, ei = map(int, input().split(' '))
     exps.append((ci, ei))
-times = []
+times = [[0] * (N + 1)]
 answer = []
-for i in range(N):
-    times.append(list(map(int, input().split(' '))))
+for i in range(1, N + 1):
+    times.append([0] + list(map(int, input().split(' '))))
+dp = [[0 for i in range(N + 1)] for i in range(T + 1)]
 
-def dfs(pos, exp, time, visited):
-    if time == T:
-        answer.append(exp)
-        return
-    if time > T:
-        return
-    for i in range(0, len(times[pos])):
-        if times[pos][i] == 0:
-            dfs(pos, exp + exps[pos][1], time + 1, visited)
-        else:
-            if exp < exps[i][0] or i in visited:
-                continue
-            visited.add(i)
-            dfs(i, exp, time + times[pos][i], visited)
-            visited.remove(i)
-for i, v in enumerate(exps):
-    if v[0] == 0:
-        dfs(i, 0, 0, set())
-print(max(answer))
+for i in range(N + 1):
+    if exps[i][0] > 0:
+        dp[0][i] = -9876543212345
+for t in range(1, T + 1):
+    for n in range(1, N + 1):
+        dp[t][n] = dp[t - 1][n] + exps[n][1]
+        for i in range(1, N + 1):
+            if n != i and t - times[i][n] >= 0 and dp[t - times[i][n]][i] >= exps[n][0]:
+                dp[t][n] = max(dp[t - times[i][n]][i], dp[t][n])
+
+print(max(dp[T]))   
+
+# def dfs(pos, exp, time, visited):
+#     if time == T:
+#         answer.append(exp)
+#         return
+#     if time > T:
+#         return
+#     for i in range(0, len(times[pos])):
+#         if times[pos][i] == 0:
+#             dfs(pos, exp + exps[pos][1], time + 1, visited)
+#         else:
+#             if exp < exps[i][0] or i in visited:
+#                 continue
+#             visited.add(i)
+#             dfs(i, exp, time + times[pos][i], visited)
+#             visited.remove(i)
+# for i, v in enumerate(exps):
+#     if v[0] == 0:
+#         dfs(i, 0, 0, set())
+# print(max(answer))
 
