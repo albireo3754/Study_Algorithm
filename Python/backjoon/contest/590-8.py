30b0d7cb4991dc3df588442f466952a451f8378e
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-02-26 21:07:28 +0900

    dict & set implemation

---
+++ Python/backjoon/contest/590-8.py
@@ -0,0 +1,54 @@
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split(' '))
+grid = []
+terra = [0, 0]
+for i in range(N):
+    grid.append(input().rstrip())
+    isterra = grid[i].find('T')
+    if isterra >= 0:
+        terra[0] = i
+        terra[1] = isterra
+        grid[i] = grid[i].replace('T', '0')
+
+distance = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+visited = [[0 for i in range(M)] for i in range(N)]
+answer = []
+def dfs(x, y, time, dx, dy):
+    if grid[x][y] == 'E':
+        answer.append(time)
+        return
+    nx, ny = x + dx, y + dy
+    time += int(grid[x][y])
+    if 0 <= nx < N and 0 <= ny < M:
+        if grid[nx][ny].isdigit():
+            ## visit check###############
+            dfs(nx, ny, time, dx, dy)
+        elif grid[nx][ny] == 'E':
+            dfs(nx, ny, time, dx, dy)
+        elif grid[nx][ny] == 'R':
+            if dx == 0:
+                newdist = distance[:2]
+            else:
+                newdist = distance[2:]
+            for dx, dy in newdist:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < N and 0 <= ny < M:
+                    if grid[nx][ny] == 'R' or grid[nx][ny] == 'H':
+                        continue
+                    dfs(nx, ny, time, dx, dy)
+for dx, dy in distance:
+    dfs(terra[0], terra[1], 0, dx,dy)
+
+print(min(answer) if len(answer) else -1)
+        # else:
+    # for di, dj in distance:
+    #     nx, ny = x + dx, y + dy
+    #     if 0 <= nx < N and 0 <= nx < M:
+            
+    #         pass
+    #     else:
+    #         return -1
+    
