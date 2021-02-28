1756bdb9ffe4ebe9dc147e13408b385b6c14bc0a
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-02-28 13:20:26 +0900

    contest

--- Python/backjoon/contest/590-5.py
+++ Python/backjoon/contest/590-5.py
@@ -13,20 +13,24 @@ i = 0
 flag = 0
 xi = pos[0] + dist[0]
 next_pi = 1
-while i < len(pos):
-    max_pi = xi 
-    while next_pi < len(pos) and xi >= pos[next_pi]:
-        max_pi = max(max_pi, pos[next_pi] + dist[next_pi])
-        next_pi += 1
-    xi = max_pi
-    i = next_pi
-
-    if max_pi >= M:
-        flag = 1
-        break
-    answer += 1
-
-if flag == 0:
-    print(-1)
+if xi >= M:
+    print(0)
 else:
-    print(answer)
+    while i < len(pos):
+        max_pi = xi 
+        while i < len(pos) and xi >= pos[i]:
+            max_pi = max(max_pi, pos[i] + dist[i])
+            i += 1
+        if i < len(pos) and max_pi < pos[i]:
+            break
+        xi = max_pi
+
+        if max_pi >= M:
+            flag = 1
+            break
+        answer += 1
+
+    if flag == 0:
+        print(-1)
+    else:
+        print(answer)
