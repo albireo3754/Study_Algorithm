5d5433d070bd243ce838dee6c5208d691771fd5f
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-02-28 15:19:02 +0900

    contest

--- Python/backjoon/contest/590-8.py
+++ Python/backjoon/contest/590-8.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10000000)
+import heapq
 input = sys.stdin.readline
 
 M, N = map(int, input().split(' '))
@@ -12,49 +12,52 @@ for i in range(N):
         terra[0] = i
         terra[1] = isterra
         grid[i] = grid[i].replace('T', '0')
+visited = [[[9999999 for i in range(2)] for i in range(M)] for i in range(N)]
+direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-distance = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-visited = [[0 for i in range(M)] for i in range(N)]
 answer = []
-def dfs(x, y, time, dx, dy):
-    if grid[x][y] == 'E':
-        answer.append(time)
-        return
-    nx, ny = x + dx, y + dy
-    time += int(grid[x][y])
-    if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0:
-        if grid[nx][ny].isdigit():
-            ## visit check###############
-            visited[nx][ny] = 1
-            dfs(nx, ny, time, dx, dy)
-            visited[nx][ny] = 0
-        elif grid[nx][ny] == 'E':
-            visited[nx][ny]
-            dfs(nx, ny, time, dx, dy)
-            visited[nx][ny]
-        elif grid[nx][ny] == 'R':
-            if dx == 0:
-                newdist = distance[:2]
-            else:
-                newdist = distance[2:]
-            for dx, dy in newdist:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < N and 0 <= ny < M:
-                    if grid[nx][ny] == 'R' or grid[nx][ny] == 'H':
+def bfs(x, y):
+    q = []
+    for i in range(4):
+        heapq.heappush(q, (0, x, y, i))
+        visited[x][y][i >> 1] = 0
+    while q:
+        time, x, y, dr = heapq.heappop(q)
+        if grid[x][y] == 'E':
+            answer.append(time)
+            return
+        if visited[x][y][dr >> 1] != time:
+            continue
+        dx, dy = direction[dr]
+        nx, ny = x + dx, y + dy
+        time += int(grid[x][y])
+        if 0 <= nx < N and 0 <= ny < M:
+            if grid[nx][ny] == 'E':
+                if visited[nx][ny][dr >> 1] > time:
+                    visited[nx][ny][dr >> 1] = time
+                    heapq.heappush(q, (time, nx, ny, i))
+            elif grid[nx][ny] == 'H':
+                continue    
+            elif grid[nx][ny] == 'R':
+                for i in range(4):
+                    if i == dr:
                         continue
-                    visited[nx][ny] = 1
-                    dfs(nx, ny, time, dx, dy)
-                    visited[nx][ny] = 0
-for dx, dy in distance:
-    dfs(terra[0], terra[1], 0, dx,dy)
+                    dx, dy = direction[i]
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M:
+                        if grid[nx][ny] == 'R' or grid[nx][ny] == 'H':
+                            continue
+                        if visited[nx][ny][i >> 1] > time:
+                            visited[nx][ny][i >> 1] = time
+                            heapq.heappush(q, (time, nx, ny, i))
+
+            else:
+                ## visit check###############
+                if visited[nx][ny][dr >> 1] > time:
+                    visited[nx][ny][dr >> 1] = time
+                    heapq.heappush(q, (time, nx, ny, dr))
+
+bfs(terra[0], terra[1])
+
 
 print(min(answer) if len(answer) else -1)
-        # else:
-    # for di, dj in distance:
-    #     nx, ny = x + dx, y + dy
-    #     if 0 <= nx < N and 0 <= nx < M:
-            
-    #         pass
-    #     else:
-    #         return -1
-    
