915378fbd67d18b20cf4f60f5ade7c05eb5f1ff3
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-03 19:58:58 +0900

    DP

--- Python/backjoon/gold/1369.py
+++ Python/backjoon/gold/1369.py
@@ -4,58 +4,31 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10000000)
 N = int(input())
 grid = [[1 for i in range(N + 1)]]
-for i in range(N):
-    grid.append([1] + list(map(int, input().split(' '))))
-
-
-# print(grid)
-grid_ = [[[999999999999, 9999999999999] for i in range(N + 1)] for i in range(N + 1)]
-for i in range(0, N + 1):
-    for j in range(0, N + 1):
-        if grid[i][j] == 0:
+Inf = 999999999999
+grid_ = [[[Inf, Inf] for i in range(N + 1)] for i in range(N + 1)]
+for i in range(1, N + 1):
+    temp_row = list(map(int, input().split(' ')))
+    for j in range(1, N + 1):
+        tj = j -1
+        if temp_row[tj] == 0:
             continue
-
-        a = 1
-        while grid[i][j] % (2 ** a) == 0:
+        a = 0
+        while temp_row[tj] % 2 == 0:
             a += 1
-        a -= 1
-
-        b = 1
-        while grid[i][j] % (5 ** b) == 0:
+            temp_row[tj] = temp_row[tj] // 2
+        b = 0
+        while temp_row[tj] % 5 == 0:
             b += 1
-        b -= 1
+            temp_row[tj] = temp_row[tj] // 5
+
+
         grid_[i][j][0] = a
         grid_[i][j][1] = b
-
 answer = []
-# print(grid_)
-# def dfs(i, j, a, b):
-#     if i == N and j == N:
-#         answer.append(min([a + grid_[i][j][0], b + grid_[i][j][1]]))
-#         return
-#     if grid_[i][j][0] == -1:
-#         return
-#     if j < N:
-#         dfs(i, j + 1, a + grid_[i][j][0], b + grid_[i][j][1])
-#     if i < N:
-#         dfs(i + 1, j, a + grid_[i][j][0], b + grid_[i][j][1])
-# dfs(1, 1, 0, 0)
-# print(min(answer))
-# print(grid_)
 for i in range(1, N + 1):
     for j in range(1, N + 1):
-        if grid[i][j] == 0:
-            continue
         if i == 1 and j == 1:
             continue
-        elif j == 1:
-            grid_[i][j][0] += grid_[i - 1][j][0]
-            grid_[i][j][1] += grid_[i - 1][j][1]
-        elif i == 1:
-            grid_[i][j][0] += grid_[i][j - 1][0]
-            grid_[i][j][1] += grid_[i][j - 1][1]
-        else:    
-            grid_[i][j][0] += min(grid_[i - 1][j][0], grid_[i][j - 1][0])
-            grid_[i][j][1] += min(grid_[i - 1][j][1], grid_[i][j - 1][1])
-# print(grid_)
+        grid_[i][j][0] += min(grid_[i - 1][j][0], grid_[i][j - 1][0])
+        grid_[i][j][1] += min(grid_[i - 1][j][1], grid_[i][j - 1][1])
 print(min(grid_[N][N]))
