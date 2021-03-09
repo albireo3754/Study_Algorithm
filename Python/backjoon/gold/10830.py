1aebdb3ebc494f898e1b56fe0a795ecdde9fc135
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-09 22:12:24 +0900

    Div Conq

--- Python/backjoon/gold/10830.py
+++ Python/backjoon/gold/10830.py
@@ -3,3 +3,29 @@ import sys
 input = sys.stdin.readline
 
 N, B = map(int, input().split(' '))
+matrix = []
+
+ans=[[0 for i in range(N)] for i in range(N)]
+for i in range(N):
+    matrix.append(list(map(int, input().split(' '))))
+    ans[i][i] = 1
+temp=[[0 for i in range(N)] for i in range(N)]
+def mul(a, b):
+    for i in range(N):
+        for j in range(N):
+            temp[i][j] = 0
+            for k in range(N):
+                temp[i][j] += (a[i][k] * b[k][j])
+            temp[i][j] %= 1000
+    for i in range(N):
+        for j in range(N):
+            a[i][j] = temp[i][j]
+            
+while B > 0:
+    if B % 2 == 1:
+        mul(ans, matrix)
+    mul(matrix, matrix)
+    B = B // 2
+
+for i in ans:
+    print(*i)
