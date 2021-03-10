292233bca99d115526f85c7644509247a0764ae3
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-10 20:48:06 +0900

    dp

--- Python/backjoon/silver/2156.py
+++ Python/backjoon/silver/2156.py
@@ -9,9 +9,6 @@ dp = [0 for _ in range(N)]
 dp[0] = grape[0]
 if N > 1:
     dp[1] = grape[0] + grape[1]
-if N > 2:
-    dp[2] = grape[2] + max(grape[0], grape[1])
-for i in range(3, N):
-    dp[i] = grape[i] + max(grape[i - 1] + dp[i - 3], dp[i - 2])
-    dp[i] = max(dp[i - 1], dp[i])
+for i in range(2, N):
+    dp[i] = max(grape[i] + grape[i - 1] + dp[i - 3], grape[i] + dp[i - 2], dp[i - 1])
 print(dp[-1])
