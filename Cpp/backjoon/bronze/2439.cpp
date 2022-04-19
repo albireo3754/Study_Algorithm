67bb4bcad0cfd9f99707647a58ff7fa6056327a0
starpro123 <starpro123@naver.com>
2022-04-20 00:45:48 +0900

    별찍기

---
+++ Cpp/backjoon/bronze/2439.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        
+        for (int j = i; j < N; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
