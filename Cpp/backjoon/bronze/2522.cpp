3f693a4df51054cf41ae99b3783c9bc73d39324e
starpro123 <starpro123@naver.com>
2022-04-27 23:18:47 +0900

    별찍기

---
+++ Cpp/backjoon/bronze/2522.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        // for (int j = 0; j < N - i - 1; j++) {
+        //     printf(" ");
+        // }
+        for (int j = N - i - 1; j < N; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = N - 2; i >= 0; i--) {
+        for (int j = 0; j < N - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = N - i - 1; j < N; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ Cpp/backjoon/bronze/2523.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = N - i - 1; j < N; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = N - 2; i >= 0; i--) {
+        for (int j = N - i - 1; j < N; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
