808fa8d1d7cac579b3e1f06563e6f91bc9998f81
alibreo3754 <starpro123@naver.com>
2023-11-12 14:22:20 +0900

    풀이를 알 수 없음

--- Cpp/backjoon/gold/2085.cpp
+++ Cpp/backjoon/gold/2085.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+<<<<<<< HEAD
+=======
+#include <vector>
+#include <algorithm>
+>>>>>>> d18aa3b (풀이를 알 수 없음)
 #include <set>
 
 using namespace std;
 
+<<<<<<< HEAD
 int result = 0;
 
 int dp(string target, int base)
@@ -43,12 +49,17 @@ int dp(string target, int base)
         chCount += result;
     }
 }
+=======
+set<vector<string>> memo = {};
+>>>>>>> d18aa3b (풀이를 알 수 없음)
 
 int main()
 {
     string s;
     cin >> s;
+    int result = 0;
 
+<<<<<<< HEAD
     for (int base_i = s.length() - 1; base_i > 0; --base_i)
     {
         if (s.at(base_i) == '0')
@@ -56,6 +67,50 @@ int main()
             continue;
         }
         dp(s.substr(0, base_i), stoi(s.substr(base_i)));
+=======
+    if (s.at(0) == '0') {
+        cin >> result;
+        return 0;
+>>>>>>> d18aa3b (풀이를 알 수 없음)
     }
+    char target = s.at(0);
+    string iter = s.substr(1);
+
+    for (int i = 1; i < s.length(); ++i) {
+        string new_s { s.at(i) };
+        while (i < s.length() - 1 && s.at(i + 1) != '0') {
+            new_s.push_back(++i);
+        }
+        if (i == s.length() - 1) {
+            break;
+        }
+        int base = stoi(s.substr(i));
+        int temp = stoi(new_s);
+        if (memo.size() == 0) {
+            if (temp < base) {
+                memo.insert({ new_s });    
+                result+= 1;
+            }
+        } else {
+            set<vector<string>> tempSet = {};
+            for (auto m: memo) {
+                for (auto _m: m) {
+                    if (stoi(_m) < base) {
+                        break;
+                    }
+                }
+                vector<string> newVector;
+                if (not break) {
+                    copy(m.begin(), m.end(), newVector);
+                }
+            }
+        }
+    }
+
+    // for (int base_i = s.length() - 1; base_i > 0; --base_i)
+    // {
+
+    // }
+    cin >> result;
     return 0;
 }
